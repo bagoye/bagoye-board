1e45e4092a45af76359380fa99f08ccae0cf782f
bagoye <dltd0912@gmail.com>
2024-09-22 16:45:04 +0900

    FE: 게시물 작성 페이지 API 연동

--- front/src/apis/index.ts
+++ front/src/apis/index.ts
@@ -4,6 +4,8 @@ import { SignInResponseDto, SignUpResponseDto } from "./response/auth";
 import { error } from "console";
 import { ResponseDto } from "./response";
 import { GetSignInUserResponseDto } from "./response/user";
+import { PostBoardRequestDto } from "./request/board";
+import { PostBoardResponseDto } from "./response/board";
 
 const DOMAIN = 'http://localhost:8080';
 
@@ -43,6 +45,22 @@ export const signUpRequest = async (requestBody: SignUpRequestDto) => {
     return result;
 }
 
+const POST_BOARD_URL = () => `${API_DOMAIN}/board`;
+
+export const postBoardRequest = async (requestBody: PostBoardRequestDto, accessToken: string) => {
+    const result = await axios.post(POST_BOARD_URL(), requestBody, authorization(accessToken))
+        .then(response => {
+            const responseBody: PostBoardResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            if (!error.response) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+        })
+    return result;
+}
+
 const GET_SIGN_IN_USER_URL = () => `${API_DOMAIN}/user`;
 
 export const getSignInUserRequest = async (accessToken: string) => {
@@ -57,4 +75,22 @@ export const getSignInUserRequest = async (accessToken: string) => {
             return responseBody;
         })
     return result
+}
+
+const FILE_DOMAIN = `${DOMAIN}/file`;
+
+const FILE_UPLAOD_URL = () => `${FILE_DOMAIN}/upload`;
+
+const multipartFormData = { headers: { 'Content-Type': 'multupart/form-data' } };
+
+export const fileUploadRequest = async (data: FormData) => {
+    const result = await axios.post(FILE_UPLAOD_URL(), data, multipartFormData)
+        .then(response => {
+            const responseBody: string = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            return null;
+        })
+    return result;
 }
