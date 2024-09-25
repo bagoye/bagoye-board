705c9af8644423444ff31d17486758e5ebe038bf
bagoye <dltd0912@gmail.com>
2024-09-25 20:52:51 +0900

    FE: 게시물 상세 페이지 이벤트 처리

---
+++ front/src/mocks/board.mock.ts
@@ -0,0 +1,14 @@
+import { Board } from "types/interface";
+
+const boardMock: Board = {
+    boardNumber: 1,
+    title: '하이하이 ~~',
+    content: '엉엉 재미이따 ㅠ',
+    boardImageList: ['https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjAy/MDAxNjQzMTAyOTk2NjE0.gw_H_jjBM64svaftcnheR6-mHHlmGOyrr6htAuxPETsg.8JJSQNEA5HX2WmrshjZ-VjmJWqhmgE40Qm5csIud9VUg.JPEG.minziminzi128/IMG_7374.JPG?type=w800', 'https://item.kakaocdn.net/do/bed74cfad27dcaaaace8df2827d4a401f604e7b0e6900f9ac53a43965300eb9a'],
+    writeDatetime: '2024. 09. 25',
+    writerEmail: 'test@test.com',
+    writerNickname: '바구',
+    writerProfileImage: 'https://item.kakaocdn.net/do/bed74cfad27dcaaaace8df2827d4a401f604e7b0e6900f9ac53a43965300eb9a',
+}
+
+export default boardMock;
