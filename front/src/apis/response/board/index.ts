6eb31cce3a42663483a4108347384a0ade72d0b3
bagoye <dltd0912@gmail.com>
2024-09-30 15:57:07 +0900

    FE: 검색 페이지 API 연동

--- front/src/apis/response/board/index.ts
+++ front/src/apis/response/board/index.ts
@@ -9,6 +9,7 @@ import DeleteBoardResponseDto from "./delete-board.response.dto";
 import PatchBoardResponseDto from "./patch-board.response.dto";
 import GetTop3BoardListResponseDto from "./get-top-3-board-list.response.dto";
 import GetLatestBoardListResponseDto from "./get-latest-board-list.response.dto";
+import GetSearchBoardListResponseDto from "./get-search-board-list.response.dto";
 
 export type {
     PostBoardResponseDto,
@@ -19,6 +20,7 @@ export type {
     GetBoardResponseDto,
     GetTop3BoardListResponseDto,
     GetLatestBoardListResponseDto,
+    GetSearchBoardListResponseDto,
     PatchBoardResponseDto,
     DeleteBoardResponseDto,
     IncreaseViewCountResponseDto
